c4c12b07023861fcd48cb7062d000638f512f6dc
айхан-гаджибалаев <ayhan.gadjibalayev@gmail.com>
2025-09-21 16:51:35 +0300

    coffee machine

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./app.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="machine">
+        <div className="block">
+          <span className="mark-name">HAROZHANG</span>
+          <div className="roll">
+            <span className="sec-text">sec</span>
+            <button className="button">STOP</button>
+          </div>
+          <div className="aaaa"></div>
+        </div>
+        <div className="black">
+          <div className="white-line"></div>
+          <div className="white-line"></div>
+          <div className="white-line"></div>
+          <div className="white-line"></div>
+          <div className="white-line"></div>
+          <div className="white-line"></div>
+          <div className="white-line"></div>
+          <div className="white-line"></div>
+          <div className="white-line"></div>
+          <div className="white-line"></div>
+          <div className="white-line"></div>
+          <div className="white-line"></div>
+          <div className="white-line"></div>
+          <div className="white-line"></div>
+          <div className="white-line"></div>
+          <div className="white-line"></div>
+          <div className="white-line"></div>
+        </div>
+      </div>
+        <div className="xxx"></div>
+      <div className="footer-machine"></div>
+      <div className="design-credit">DESIGN BY HAROZHANG</div>
     </div>
   );
 }
